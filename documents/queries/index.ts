daf30a849f5007c4abfb1040ea7bc03417f73f45
ayaanqui <ayaanqui.com@gmail.com>
2025-11-27 02:34:05 -0600

    feat: export all query modules

--- documents/queries/index.ts
+++ documents/queries/index.ts
@@ -1,4 +1,31 @@
-import * as Admin from './admin';
-import * as GroceryList from './grocery-list';
+import * as Admin from "./admin";
+import * as Branch from "./branch";
+import * as Brand from "./brand";
+import * as Category from "./category";
+import * as GroceryList from "./grocery-list";
+import * as List from "./list";
+import * as Metadata from "./metadata";
+import * as Price from "./price";
+import * as ProductNutrition from "./product-nutrition";
+import * as Product from "./product";
+import * as Search from "./search";
+import * as Stock from "./stock";
+import * as Store from "./store";
+import * as User from "./user";
 
-export { Admin, GroceryList };
+export {
+  Admin,
+  Branch,
+  Brand,
+  Category,
+  GroceryList,
+  List,
+  Metadata,
+  Price,
+  ProductNutrition,
+  Product,
+  Search,
+  Stock,
+  Store,
+  User,
+};
