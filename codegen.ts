fe4b183ccef11ea77cb6bfa708f4760cc4166028
ayaanqui <ayaanqui.com@gmail.com>
2025-12-03 20:57:42 -0600

    build: fix dependencies

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   schema: process.env.EXPO_PUBLIC_API_URL ?? 'http://localhost:8080/graphql',
-  documents: ['documents/**/*.ts'],
+  documents: ['src/documents/**/*.ts'],
   generates: {
     'types/': {
       preset: 'client',
